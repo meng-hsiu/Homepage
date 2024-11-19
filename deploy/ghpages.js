77f77d20e1dda9b6f3f186b014e748242b37b850
menghsiu <menghsiu0120@gmail.com>
2024-11-20 00:29:19 +0800

    修改了ghpages.js

--- deploy/ghpages.js
+++ deploy/ghpages.js
@@ -1,20 +1,23 @@
-// place at : deploy/ghpages.js
-// you can see more info at https://github.com/tschaub/gh-pages
-const path = require('path');
-const ghpages = require('gh-pages');
+// 檔案路徑：deploy/ghpages.js
+// 參考：https://github.com/tschaub/gh-pages
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+import ghpages from 'gh-pages';
+
+// 取得 __dirname（在 ES 模組中需自行計算）
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const options = {
     branch: 'gh-pages',
-    repo: 'https://github.com/meng-hsiu/Homepage.git' // project github repo
+    repo: 'https://github.com/meng-hsiu/Homepage.git', // 專案的 GitHub repo
 };
 
-const callback = err => {
-
+const callback = (err) => {
     if (err) console.error(err);
-    else console.log('publish success');
+    else console.log('Publish success');
 };
 
-/**
- * This task pushes to the `master` branch of the configured `repo`.
- */
+// 部署至 gh-pages 分支
 ghpages.publish(path.resolve(__dirname, '../dist'), options, callback);
